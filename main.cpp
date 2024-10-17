fc5d12b22c7d5bd3b1f3789e5da0f4ffd6a963af
Pat Chen <patchen21@gmail.com>
2024-10-16 21:35:55 -0400

    first

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+// Print "Hello World"
+// int main() {
+//     int file_size = 0;
+//     int counter = 0;
+//     double sales = 9.99;
+//     // std::cout << "Hello World!";
+//     std::cout << file_size;
+//     return 0;
+// }
+
+// Variables
+// int main() {
+//     int a = 1;
+//     int b = 2;
+//     int temp = a;
+//     a = b;
+//     b = temp;
+//     std::cout << a;
+//     return 0;
+// }
+
+// Constants
+// int main() {
+//     const double pi = 3.14;
+//     pi = 0;
+//     return 0;
+// }
+
+// Naming Conventions
+int main() {
+    int file_size; // Snake Case
+    int FileSize; // Pascal Case
+    int fileSize; // Camel Case
+    int iFileSizes; // HUngarian Notation
+    return 0;
+}
+
+
+
